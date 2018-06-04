be9b8fcc61c8c49f90f4eacce854bf4b08acd8b5
Alexandro <alejo_alvarez67@hotmail.com>
2018-06-04 14:04:38 +0200

    page contact et mentions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'mentions', to: 'home#mentions'
 end
