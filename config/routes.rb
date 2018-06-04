87fc8a38ee9dcbafa6dfe7221a1bb77318964921
Louis-Elie Maillard <louiseliemaillard@gmail.com>
2018-06-04 14:26:54 +0200

    CSS ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  root 'home#index'
-  get 'home/contact'
+	root 'home#index'
+  	devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
