65ed4ece4eea054b5699343a861e843bbbf18705
guillaume <g.delory62@gmail.com>
2018-06-05 23:40:11 +0200

    dernier push avant divorce

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-	belongs_to :home_team, :foreign_key => 'home_team', :class_name => 'Team'
-  	belongs_to :visiting_team, :foreign_key => 'visiting_team', :class_name => 'Team'
+	belongs_to :home_team, :foreign_key => 'home_team_id', :class_name => 'Team'
+  	belongs_to :visiting_team, :foreign_key => 'visiting_team_id', :class_name => 'Team'
   	has_many :events
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ApplicationRecord
-	has_many :home_games, foreign_key: 'home_team', class_name: 'Game'
-	has_many :visiting_games, foreign_key: 'visiting_team', class_name: 'Game'
+	has_many :games, class_name: 'Game', foreign_key: 'home_team'
+	has_many :games, class_name: 'Game', foreign_key: 'visiting_team'
 end
 
 
