6ae2b84bc4babcc78886c3ed59098de9d8de5db4
guillaume <g.delory62@gmail.com>
2018-06-05 23:25:07 +0200

    la folie

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ApplicationRecord
-	has_many :home_games, class_name: 'Game', foreign_key: 'home_team'
-	has_many :visiting_games, class_name: 'Game', foreign_key: 'visiting_team'
+	has_many :home_games, foreign_key: 'home_team', class_name: 'Game'
+	has_many :visiting_games, foreign_key: 'visiting_team', class_name: 'Game'
 end
 
 
