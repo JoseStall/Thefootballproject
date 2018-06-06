76aafaf892bb2e638fe292321d1052646261dc18
guillaume <g.delory62@gmail.com>
2018-06-06 10:35:21 +0200

    test heroku

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
 
-	belongs_to :home_team, :foreign_key => 'home_team_id', :class_name => 'Team'
-  	belongs_to :visiting_team, :foreign_key => 'visiting_team_id', :class_name => 'Team'
+	belongs_to :home_team, :class_name => 'Team'
+  	belongs_to :visiting_team, :class_name => 'Team'
   	has_many :events
 end
 
