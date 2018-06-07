2bf3e05a569762a73af892eae7aac5162054fce2
guillaume <g.delory62@gmail.com>
2018-06-07 19:23:23 +0200

    push heroku

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,10 @@
 class Event < ApplicationRecord
+	geocoded_by :adress
+	after_validation :geocode
 	has_and_belongs_to_many :users
 	belongs_to :user
 	belongs_to :game
 	has_many_attached :images
 	scope :with_eager_loaded_images, -> { eager_load(images_attachments: :blob) }
-	geocoded_by :adress
-	after_validation :geocode
+
 end
