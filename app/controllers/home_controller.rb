cc87b1d4ee8f9048d5d3986fcf4d88cdb7890d9f
guillaume <g.delory62@gmail.com>
2018-06-11 14:47:00 +0200

    heroku test map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,53 +15,13 @@ class HomeController < ApplicationController
   	@teams = Team.all
   end
 
-  def result
-
-  	
-
+  def result	
   	@bars = Bar.all
-    @addresses = []
-    @description = []
-    @latitude = []
-    @longitude = []
-    @name = []
-    @bar = Bar.first
-    
-    @bars.each do |b|
-      if b.latitude != nil
-	      @addresses << b.address 
-	      @description << b.description
-	      @name << b.name
-	      @latitude << b.latitude
-	      @longitude << b.longitude
-      else
-        b.geocode
-        b.save
-    	end
-  	end
-  	p '------'
-  	p @bar.nearbys(1)
+    p '------'
   	p '------'
   	p 'longitude'
   	p @longitude
-
    @events = Event.all
-   p @events
-    @eaddresses = []
-    @edescription = []
-    @elatitude = []
-    @elongitude = []
-    @ename = []
-    @event = Event.first
-    
-    @events.each do |b|
-      if b.latitude != nil
-      @eaddresses << b.address 
-      @edescription << b.description
-      @elatitude << b.latitude
-      @elongitude << b.longitude
-    end
-  end
   end
   
 end
