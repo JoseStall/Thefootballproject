0e1667d319cedc5f86f049fbbecb5310b8f75bd7
ampueropierre <ampuero.pierre@gmail.com>
2018-06-12 15:08:12 +0200

    add review

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -6,15 +6,31 @@ class ReviewController < ApplicationController
   	p params
   	p '*********'
 
-  	tab = []
-  	params.each do |params|
-  	tab << params
-  	end
-  	puts "tab"
-  	p tab
-  
-  	#Review.create(for_user_id: @participant.first.id, by_user_id: current_user.id, stars: tab[1][1], content: tab[1][1])	
-
+  	if @participant.include?(current_user)
+  		if params[:utf8] != nil
+  			Review.create(user_id: @event.user_id, author:current_user.firstname , stars: params[:note], content: params[:message])
+  			redirect_to profil_path
+  		end
+  	else
+	  	if params[:utf8] == nil
+		  	
+		else
+		  	tab = []
+		  	params.each do |params|
+		  	tab << params
+		  	end
+		  	puts "tab"
+		  	p tab
+		  	i=0
+		  	p @participant
+		  	@participant.each do |p|
+		  		i+=1
+		  		Review.create(user_id: p.id, author:current_user.firstname , stars: tab[i][1].to_i, content: tab[i+1][1])	
+		  		i += 1
+		  	end
+		  	redirect_to profil_path
+		end
+	end
   	
   end
 end
