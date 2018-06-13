c1516b1c1266c8eae060327f9b5341e834c9a2d7
guillaume <g.delory62@gmail.com>
2018-06-13 22:21:46 +0200

    test region

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,3 +10,4 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :phone_number])        
     end
 end
+
