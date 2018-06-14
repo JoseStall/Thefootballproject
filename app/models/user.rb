ed16ea50a00bdfc5453971312c33987b2730d9fd
guillaume <g.delory62@gmail.com>
2018-06-14 10:21:57 +0200

    devise registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
 
 	before_save :capitalize_names
 
+
 	def capitalize_names
 		if self.firstname != nil && self.lastname != nil
 			self.firstname = firstname.camelcase
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,11 @@ Rails.application.routes.draw do
   get 'actus' , to: 'jeux#actu', as: 'news'
   get 'jeux/classement'
   get 'profil', to: 'user#show', as: 'profil'
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" },:controllers => { registrations: "registrations" }
   root 'home#index'
   get 'contact', to: 'home#contact', as: 'contact'
   get 'mentions', to: 'home#mentions', as: 'mentions'
   get 'search', to: 'home#search'
-
   get 'welcome/:id', to: 'events#welcome', as: 'welcome'
   get 'result', to: 'home#result', as: 'result'
   get 'validation/:id', to: 'events#validatemail', as: 'validation'
