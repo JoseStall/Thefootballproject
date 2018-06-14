3c61a6f0514d8b50b21b971dd5e0bcc683220d7c
Gauthier Masse <masse.gauthier@gmail.com>
2018-06-14 14:53:57 +0200

    title

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -146,7 +146,7 @@ class EventsController < ApplicationController
    # Never trust parameters from the scary internet, only allow the white list through.
    def event_params
 
-     params.require(:event).permit(:description, :date, :user_id, :game_id, :smoke, :address, :food, :drink, :invited, images: [])
+     params.require(:event).permit(:title, :description, :date, :user_id, :game_id, :smoke, :address, :food, :drink, :invited, images: [])
 
    end
 end
