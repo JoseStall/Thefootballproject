bf78569a3a19c4863c6cf6360256651e86b2bfa4
ampueropierre <ampuero.pierre@gmail.com>
2018-06-15 11:55:48 +0200

    change bar search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,7 +16,16 @@ class HomeController < ApplicationController
   def result	
     @barsall = Bar.all
     @eventsall = Event.all
+    @teamall = Team.all
     @bars = @barsall.near(params[:adress] , 20)
     @events = @eventsall.near(params[:adress], 20)
+    if params[:team] != nil
+      idteam = params[:team]
+      @teamsearch = @teamall.find(idteam).name
+      p '-------------'
+      p idteam
+      p @teamsearch
+      p '-------------'
+    end
   end
 end
